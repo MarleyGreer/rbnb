8fffde2040aad99836990e624c00b7f0624e0797
Emily Truong <truong.emily@hotmail.com>
2020-09-05 15:42:25 +1000

    generated review booking and garment models

---
+++ db/migrate/20200905054028_create_bookings.rb
@@ -0,0 +1,15 @@
+class CreateBookings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookings do |t|
+      t.datetime :start_date
+      t.datetime :end_date
+      t.string :status
+      t.integer :total_price
+      t.references :review, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :garment, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
