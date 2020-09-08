2826d160df874b6b2d66dbb5c0fcc0840c547e37
Marley Greer <marley.greer93@gmail.com>
2020-09-08 19:39:11 +1000

    added basic crud actions on garments controller

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -1,2 +1,43 @@
 class GarmentsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def home
+  end
+
+  def index
+    @garments = Garment.all
+  end
+
+  def show
+    @garment = Garment.find(params[:id])
+  end
+
+  def new
+    @garment = Garment.new
+  end
+
+  def edit
+  end
+
+  def update
+    @garment.update(garment_params)
+  end
+
+  def create
+    @garment = Garment.new(garment_params)
+  end
+
+  def destroy
+    @garment.destroy
+  end
+
+  private
+
+  def set_garment
+    @garment = Garment.find(params[:id])
+  end
+
+  def garment_params
+    params.require(:garment).permit()
+  end
 end
