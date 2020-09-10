e3eed0652ff45b168b57b4b7dc7c50ec68d6e3f8
Trent Skurrie <trent.skurrie@jacmax.com.au>
2020-09-10 21:43:53 +1000

    index, shows and links updated

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,12 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+const showoncardclick = () => {
+  const card = document.querySelector('.card');
+    window.addEventListener('click', () => {
+      $.get("<%= garment_path(garment) %>");
+    });
+}
+
+export { showoncardclick };
