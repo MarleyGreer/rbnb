10c7379b488c829a37c2afecdbd6b9e34b28f290
Emily Truong <truong.emily@hotmail.com>
2020-09-11 16:44:55 +1000

    created validation for end date can't be before start date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,18 @@
 class Booking < ApplicationRecord
-  validates :status, inclusion: { in: ["Pending", "Accepted", "Declined", "Cancelled"] }
-  has_many :reviews, dependent: :destroy
   belongs_to :user
   belongs_to :garment
+  has_many :reviews, dependent: :destroy
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :status, inclusion: { in: ["Pending", "Accepted", "Declined", "Cancelled"] }
+  validate :end_date_after_start_date
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
 end
