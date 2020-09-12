7c4a0b4c86f4ceda8e04cd6b6c93be639d123f0a
anurag sharma <anurag.vashist@gmail.com>
2020-09-12 11:16:44 +1000

    added geocoding skeleton

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -7,13 +7,22 @@ class GarmentsController < ApplicationController
   def index
     if params[:category]
       garments = Garment.where(category = params[:category])
-    else     
+    else
     @garments = Garment.all
     end
+
+      @markers = @garments.geocoded.map do |garment|
+      {
+        lat: garment.latitude,
+        lng: garment.longitude
+      }
+    end
+
   end
 
   def show
     @garment = Garment.find(params[:id])
+
   end
 
   def new
@@ -47,6 +56,7 @@ class GarmentsController < ApplicationController
 
   def set_garment
     @garment = Garment.find(params[:id])
+
   end
 
   def garment_params
