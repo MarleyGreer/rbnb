9951720b4b3525f3631c0145148f8acdff5b6121
anurag sharma <anurag.vashist@gmail.com>
2020-09-12 15:20:53 +1000

    added seed file and deleted geocoding stuff

--- app/models/garment.rb
+++ app/models/garment.rb
@@ -1,16 +1,20 @@
+
+
 class Garment < ApplicationRecord
+  VALID_SIZES = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :category, presence: true, inclusion: { in: ["Dress", "Top", "Knitwear", "Skirt", "Pant", "Jacket", "Denim", "Shoes", "Accessories"] }
-  validates :size, presence: true, inclusion: { in: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] }
+  validates :size, presence: true, inclusion: { in: VALID_SIZES }
   validates :colour, presence: true
   validates :address, presence: true
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
   has_many_attached :photos, dependent: :destroy
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
 
 end
